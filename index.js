7ab5d2ae4e7fb9af43868b6f76c472a5b82b219f
Manuel Baez Diaz <baezdiazm@yahoo.com>
2022-03-09 17:49:49 -0500

    commit tree c0ccde406c5b67bd3b0b84243013dae02726e1a4

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str) {
+    const string = str.split('')
+    string[0] = string[0].toUpperCase()
+    return string.join('')
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, "");
+  }
+
+  static titleize( phrase ) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let words = phrase.split( " " )
+    for ( let i = 0; i < words.length; i++ ) {
+      if ( i == 0 ) {
+        result.push( this.capitalize( words[ i ] ) )
+      } else {
+        if ( exceptions.includes( words[ i ] ) ) {
+          result.push( words[ i ] )
+        } else {
+          result.push( this.capitalize( words[ i ] ) )
+        }
+      }
+
+    }
+    return result.join( " " );
+  }
+
 }
